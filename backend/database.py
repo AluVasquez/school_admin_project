2c19202292ea525d6ab6d450a17644789b60ffa8
AluVasquez <alusoporte@gmail.com>
2025-07-06 15:28:56 -0400

    cosas del backend que faltaron

--- backend/database.py
+++ backend/database.py
@@ -9,12 +9,12 @@ SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False} 
+    #connect_args={"check_same_thread": False} 
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-def create_db_tables():
-    Base.metadata.create_all(bind=engine)
+#def create_db_tables():
+    #Base.metadata.create_all(bind=engine)
